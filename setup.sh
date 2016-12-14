8bb7933e3e019d0f644cb2dedbfdee31ce5da034
sankalpj <sankalp@goodbox.in>
2016-12-14 23:37:41 +0530

    added db file to gitignore and created setup.sh to setup environment

---
+++ setup.sh
@@ -0,0 +1,5 @@
+sudo apt-get install -y git python python-pip python-dev libssl-dev libmysqlclient-dev libffi-dev libjpeg-dev libcurl4-openssl-dev libmagickwand-dev
+sudo pip install virtualenvwrapper
+mkdir venv
+virtualenv venv/
+source venv/bin/activate && sudo pip install -r requirements.txt
